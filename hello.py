356b470ac64ffc2a9f22eb98c2a271e303c9cba2
Rhys Rushton <rhysworker@gmail.com>
2022-04-20 10:50:18 +1000

    basic structure

---
+++ hello.py
@@ -0,0 +1,15 @@
+from tkinter import *
+from tkinter import ttk
+
+class Compare_Data:
+
+    def __init__(self, root):
+        root.title('Data Comparison')
+
+
+
+
+
+root = Tk()
+Compare_Data(root)
+root.mainloop()
